94a5a82f84be6d71335a5e46716f31f31c50f1db
westone034626 <dydrlaks@likelion.org>
2023-10-15 19:49:50 +0900

    feat: Handle error

--- src/index.js
+++ src/index.js
@@ -16,12 +16,18 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('안녕하세요.222');
+    // res.send('안녕하세요.222');
+    throw new Error('It is an error.');
 });
 
 app.post('/', (req, res) => {
     console.log(req.body);
     res.json(req.body);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.send(error.message || '서버 에러 발생');
 });
 
 app.use(express.static(path.join(__dirname, '../uploads')));
