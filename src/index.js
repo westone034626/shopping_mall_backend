6b55d8efc0487b7cde778e6801d89e35752a49d5
westone034626 <dydrlaks@likelion.org>
2023-10-15 18:58:13 +0900

    chore: Make handle cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('안녕하세요.222');
 });
