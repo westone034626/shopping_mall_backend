1e86ad31f9d70623454b7c9105a188079a16eb7b
westone034626 <dydrlaks@likelion.org>
2023-10-15 18:55:46 +0900

    feat: Make handle requesting asset file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 4000;
@@ -6,6 +7,8 @@ const port = 4000;
 app.get('/', (req, res) => {
     res.send('안녕하세요.222');
 });
+
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 app.listen(port, () => {
     console.log('4000 port');
