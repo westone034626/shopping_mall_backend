85a2dcb5137e96c49d3138f447241398120c9b37
westone034626 <dydrlaks@likelion.org>
2023-10-15 19:09:59 +0900

    feat: Make handle requesting with json

--- src/index.js
+++ src/index.js
@@ -6,8 +6,15 @@ const app = express();
 const port = 4000;
 
 app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('안녕하세요.222');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 });
 
 app.use(express.static(path.join(__dirname, '../uploads')));
