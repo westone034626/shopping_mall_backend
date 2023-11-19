3d65fac84f65610b14dcb4711c678f9f5c01ede5
westone034626 <dydrlaks@likelion.org>
2023-11-19 23:12:48 +0900

    feat: Handle request about /products/image post

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const auth = require('../middleware/auth');
 const Product = require('../models/Product');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    }
+});
+
+const upload = multer({ storage: storage }).single('file');
 
 router.post('/', auth, async (req, res, next) => {
     try {
@@ -9,6 +21,20 @@ router.post('/', auth, async (req, res, next) => {
         await post.save();
 
         return res.sendStatus(201);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/image', auth, async (req, res, next) => {
+    try {
+        upload(req, res, (err) => {
+            if (err) {
+                return req.status(500).send(err);
+            }
+            return res.json({ fileName: res.req.file.filename });
+        });
+
     } catch (error) {
         next(error);
     }
