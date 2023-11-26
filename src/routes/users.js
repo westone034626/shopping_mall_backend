bab2e5d2c908f441ff525b17a128dfbbb58d82bc
westone034626 <dydrlaks@likelion.org>
2023-11-26 19:30:20 +0900

    feat: Delete specific cart data of request user

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const Product = require('../models/Product');
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/auth');
 
@@ -99,6 +100,36 @@ router.post('/cart', auth, async (req, res, next) => {
 
             return res.status(201).send(user.cart);
         }
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.delete('/cart', auth, async (req, res, next) => {
+    try {
+        const userInfo = await User.findOneAndUpdate(
+            { _id: req.user._id },
+            {
+                "$pull": {
+                    "cart": {
+                        "id": req.query.productId,
+                    }
+                }
+            },
+            { new: true }
+        );
+
+        const cart = userInfo.cart;
+        const array = cart.map(item => item.id);
+
+        const productInfo = await Product
+            .find({ _id: { $in: array } })
+            .populate('writer');
+
+        return res.json({
+            productInfo,
+            cart
+        });
     } catch (error) {
         next(error);
     }
